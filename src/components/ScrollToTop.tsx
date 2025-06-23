b6b7a32277fa2faee3b3787fca9daa57a6ceb3c9
Senne Dwt <Sennedewit16@icloud.com>
2025-06-23 20:49:46 +0200

    Add Google Analytics tracking in ScrollToTop component for pageview tracking on route changes

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,11 +1,23 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+  }
+}
+
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    // Google Analytics pageview bij elke routewijziging
+    if (typeof window.gtag === 'function') {
+      window.gtag('config', 'G-FC2XYC7H3M', {
+        page_path: pathname,
+      });
+    }
   }, [pathname]);
 
   return null;
